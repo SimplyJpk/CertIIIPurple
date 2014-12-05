5c1f0c40f723e743133d7cd5ff807f037a023742
LWMcClymont <lachlanmcclymont25@gmail.com>
2014-12-05 19:40:48 +1100

    CrossHairs !

---
+++ TeamPurpleProject/Assets/Lachlan/CrossHairs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossHairs : MonoBehaviour {
+		
+	public Texture2D crossHairs;
+
+	void OnGUI ()
+	{
+		GUI.DrawTexture (new Rect (Screen.width / 2 - 25, Screen.height / 2 - 25, 50, 50), crossHairs);
+	}
+}
