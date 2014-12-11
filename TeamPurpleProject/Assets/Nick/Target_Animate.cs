41709489b9551f5ee5ef9868ac64e9f1a643367d
Nickmp3z <nicholasg@internode.on.net>
2014-12-11 16:58:48 +1100

    Hopefully Level Unity scene works properly

---
+++ TeamPurpleProject/Assets/Nick/Target_Animate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target_Animate : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetTrigger ("Target_Animate");
+	}
+}
