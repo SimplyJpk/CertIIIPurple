983df535ec058943754a97d15b21cfef468861f7
Nickmp3z <nicholasg@internode.on.net>
2014-12-18 19:43:16 +1100

    Prefab adding.

---
+++ TeamPurpleProject/Assets/Nick/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed;
+	/*bool moveRight = false;
+
+	float timer = 0.5f;
+	float delay = 2.0f;*/
+
+	void Update()
+	{
+		transform.Translate(Vector3.right);
+		/*timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			timer = delay;
+			moveRight = !moveRight;
+		}
+		if (moveRight == false)
+			transform.Translate (-speed * Time.deltaTime, 0, 0);
+		else
+			transform.Translate (speed * Time.deltaTime, 0, 0 );
+			*/
+	}
+
+
+
+
+}
---
+++ TeamPurpleProject/Assets/Nick/Target_Move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target_Move : MonoBehaviour {
+
+	public float speed;
+	bool moveRight = false;
+	
+	float timer = 0.5f;
+	float delay = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer -= Time.deltaTime;
+				
+		if (timer <= 0) {
+			timer = delay;
+			moveRight = !moveRight;
+
+		}
+		if (moveRight == false)
+			transform.Translate (-speed * Time.deltaTime, 0, 0);
+		else
+			transform.Translate (speed * Time.deltaTime, 0, 0 );
+	}
+}
