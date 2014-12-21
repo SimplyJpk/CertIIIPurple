fbf2e293bbb7c2dfb045d128f63fba15447ab591
LWMcClymont <lachlanmcclymont25@gmail.com>
2014-12-21 18:50:28 +1100

    shoots forward relative to the character

--- TeamPurpleProject/Assets/James/PlayerProto_Test.cs
+++ TeamPurpleProject/Assets/James/PlayerProto_Test.cs
@@ -7,14 +7,14 @@ public class PlayerProto_Test : MonoBehaviour
     public float shootDelay = 0.4f;
     private float timer;
     private GameObject particles;
-    private Transform Camera;
+    private Transform camera;
     GameObject _obj;
 
     void Start()
     {
         Screen.lockCursor = true;
         particles = Resources.LoadAssetAtPath("Assets/Lachlan/enemyParticles.prefab", typeof(GameObject)) as GameObject;
-        Camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
 
 
 
@@ -30,7 +30,7 @@ public class PlayerProto_Test : MonoBehaviour
         if (timer < 0 && Input.GetMouseButtonDown(0))
         {
             timer = shootDelay;
-            Ray ray = new Ray(Camera.position, Camera.forward);
+            Ray ray = new Ray(camera.position, camera.forward);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
@@ -40,7 +40,7 @@ public class PlayerProto_Test : MonoBehaviour
                 if (hit.collider.gameObject.tag == "Target")
                 {
 
-                    hit.collider.gameObject.rigidbody.AddForceAtPosition(Vector3.forward * 5, hit.point, ForceMode.Impulse);
+                    hit.collider.gameObject.rigidbody.AddForceAtPosition(camera.transform.forward * 5, hit.point, ForceMode.Impulse);
                     Destroy(hit.collider.transform.parent.gameObject, 1);
 
                 }
