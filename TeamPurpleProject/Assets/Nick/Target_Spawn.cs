46fca3f535f28daa46853a00d0b0702fdc1bdf01
Nickmp3z <nicholasg@internode.on.net>
2014-12-23 16:52:49 +1100

    Added spawnpoint Prefab and spawn script. Need to make sure we alter spawn time for each gameobject created in the scene.

---
+++ TeamPurpleProject/Assets/Nick/Target_Spawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target_Spawn : MonoBehaviour {
+
+	public GameObject target;
+	float time;
+	public float spawn;
+	public float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+		time = Random.Range(spawn, spawnTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		if ( time < 0)
+		{
+			Instantiate (target, this.transform.position, Quaternion.identity);
+			Destroy (this);
+		}
+	
+	}
+}
