3155f4b804c5b0b59abe3a6a832d6df4cd416c25
James Kellie <Jpk_GameID@hotmail.com>
2014-12-29 18:37:59 +1100

    Added basic UI (Need improvements) Accidentally Deleted my target_edit.prefab Scripts so they don't have scripts to move them. I'll re-make that some point later. clip and ammo are both displayed in top left now. (Needs Improvements)
    Didn't add crouch like i said i would. Mind you, that's fairly minor. c:

--- TeamPurpleProject/Assets/James/PlayerProto_Test.cs
+++ TeamPurpleProject/Assets/James/PlayerProto_Test.cs
@@ -10,14 +10,18 @@ public class PlayerProto_Test : MonoBehaviour
     private Transform camera;
     GameObject _obj;
 
+
+    public bool _reloading = false;
+
+    public int clip = 0;
+    public int clipSize = 7;
+    public int ammo = 17;
+
     void Start()
     {
         Screen.lockCursor = true;
         particles = Resources.LoadAssetAtPath("Assets/Lachlan/enemyParticles.prefab", typeof(GameObject)) as GameObject;
         camera = GameObject.FindGameObjectWithTag("MainCamera").transform;
-
-
-
     }
 
     void Awake()
@@ -26,29 +30,62 @@ public class PlayerProto_Test : MonoBehaviour
     }
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer < 0 && Input.GetMouseButtonDown(0))
+        if (timer > 0) timer -= Time.deltaTime;
+        
+       CheckInput();
+
+        if (timer < 0 && Input.GetMouseButtonDown(0) && _reloading == false)
         {
             timer = shootDelay;
-            Ray ray = new Ray(camera.position, camera.forward);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (clip > 0)
             {
-                GameObject explosion = Instantiate(particles, hit.point, Quaternion.identity) as GameObject;
-                Destroy(explosion, 3);
-
-                if (hit.collider.gameObject.tag == "Target")
+                clip--;
+                Ray ray = new Ray(camera.position, camera.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
                 {
+                    GameObject explosion = Instantiate(particles, hit.point, Quaternion.identity) as GameObject;
+                    Destroy(explosion, 3);
 
-                    hit.collider.gameObject.rigidbody.AddForceAtPosition(camera.transform.forward * 5, hit.point, ForceMode.Impulse);
-                    Destroy(hit.collider.transform.parent.gameObject, 1);
+                    if (hit.collider.gameObject.tag == "Target")
+                    {
+
+                        hit.collider.gameObject.rigidbody.AddForceAtPosition(camera.transform.forward * 5, hit.point, ForceMode.Impulse);
+                        Destroy(hit.collider.transform.parent.gameObject, 1);
+
+                    }
+
+
 
                 }
-
-
             }
 
         }
+        Debug.Log(timer);
+    }
 
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.R) && _reloading == false)
+        {
+            Debug.Log("Reloading Started");
+            timer = 2f;
+            _reloading = true;
+        }
+        else if (_reloading == true && timer <= 0)
+        {
+            if (ammo < 7)
+            {
+                clip = ammo;
+                ammo = 0;
+            }
+            else
+            {
+                clip = clipSize;
+                ammo -= clipSize;
+            }
+            _reloading = false;
+            Debug.Log("Reloading Finished");
+        }
     }
 }
---
+++ TeamPurpleProject/Assets/James/UI/UInterface.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UInterface : MonoBehaviour {
+
+   void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 30), "Ammo: " + GameObject.Find("Player").GetComponent<PlayerProto_Test>().ammo);
+        GUI.Label(new Rect(0, 30, 100, 30), "Clip: " + GameObject.Find("Player").GetComponent<PlayerProto_Test>().clip);
+      if (GameObject.Find("Player").GetComponent<PlayerProto_Test>()._reloading)
+      {
+          GUI.Label(new Rect(0, 50, 100, 30), "Reloading");
+      }
+    }
+}
