d90470cdf7fb65c36f4c2968e89ea7651ed3d18c
James Kellie <Jpk_GameID@hotmail.com>
2015-01-14 11:17:40 +1100

    Fixed typo

--- TeamPurpleProject/Assets/Nick/Target_Move.cs
+++ TeamPurpleProject/Assets/Nick/Target_Move.cs
@@ -6,22 +6,19 @@ public class Target_Move : MonoBehaviour {
 	public float speed;
 	bool moveRight = false;
 	
-	float timer = 0.5f;
+	float timer = 1f;
 	float delay = 2.0f;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		timer -= Time.deltaTime;
 				
 		if (timer <= 0) {
 			timer = delay;
 			moveRight = !moveRight;
-
 		}
 		if (moveRight == false)
 			transform.Translate (-speed * Time.deltaTime, 0, 0);
