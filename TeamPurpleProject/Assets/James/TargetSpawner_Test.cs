f5bf50b446c42d8d6d56f92400dc9e38accdec46
James Kellie <Jpk_GameID@hotmail.com>
2015-01-14 11:20:11 +1100

    Missed File

---
+++ TeamPurpleProject/Assets/James/TargetSpawner_Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetSpawner_Test : MonoBehaviour
+{
+
+    public GameObject target;
+    public float time;
+
+    void Update()
+    {
+        if (time > 0) time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Instantiate(target, transform.position, Quaternion.identity);
+            time = 0;
+        }
+    }
+}
